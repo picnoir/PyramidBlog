b0edd9e9b4d1f2ab11f033da57a6acfda7600164
Félix Baylac-Jacqué <baylac.felix@gmail.com>
2013-08-01 13:56:51 +0200

    Fixing package path.

--- blog/importArticle.py
+++ blog/importArticle.py
@@ -2,7 +2,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from models import Article, create_db_tables
+from blog.models import Article, create_db_tables
 
 import sys
 
